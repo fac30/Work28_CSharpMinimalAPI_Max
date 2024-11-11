644f53f138f0084441e90a203b887f5b553209ab
maxitect <maxime.downe@gmail.com>
2024-11-11 17:55:27 +0000

    build: install sqlite and EF core tools and design-time logic dependencies module 2

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
@@ -41,24 +42,24 @@ app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsyn
 
 app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
 {
-      var pizza = await db.Pizzas.FindAsync(id);
-      if (pizza is null) return Results.NotFound();
-      pizza.Name = updatepizza.Name;
-      pizza.Description = updatepizza.Description;
-      await db.SaveChangesAsync();
-      return Results.NoContent();
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
 
 app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
 {
-   var pizza = await db.Pizzas.FindAsync(id);
-   if (pizza is null)
-   {
-      return Results.NotFound();
-   }
-   db.Pizzas.Remove(pizza);
-   await db.SaveChangesAsync();
-   return Results.Ok();
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null)
+    {
+        return Results.NotFound();
+    }
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
 });
 
 app.Run();
